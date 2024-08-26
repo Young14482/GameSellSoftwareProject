f6f79a9d9a72ab19a36a95c0c199386255da30a5
LHS <LHS@424>
2024-08-26 14:47:56 +0900

    [feat] Mapper 추가 GameDAO 검색 추가 중
    GameMapper, UserMapper 추가
    GameDAO에 옵션을 통해 게임 리스트 불러오는 메서드 구현 중

---
+++ JojoSoft/src/user/UserMapper.java
@@ -0,0 +1,29 @@
+package user;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import materials.IResultMapper;
+
+public class UserMapper implements IResultMapper<User> {
+
+	@Override
+	public User resultMapping(ResultSet rs) {
+		try {
+			String userid = rs.getString("user_id");
+			String userpw = rs.getString("user_pw");
+			String userNickName = rs.getString("user_nickname");
+			String userGrade = rs.getString("user_grade");
+			String userBirth = rs.getString("user_birth");
+			String userPhoneNumber = rs.getString("user_phonenumber");
+			int userUsedCash = rs.getInt("user_usedcash");
+
+			return new User(userid, userpw, userNickName, userGrade, userBirth, userPhoneNumber, userUsedCash);
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+			throw new RuntimeException("User 매핑 중 예외 발생", e);
+		}
+	}
+
+}
